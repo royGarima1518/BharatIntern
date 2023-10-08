437b1a9be09d94f9166ec44be5d8850bc627905c
royGarima1518 <145840106+royGarima1518@users.noreply.github.com>
2023-10-08 19:42:19 +0530

    Add files via upload

---
+++ BharatIntern2/temperature.js
@@ -0,0 +1,11 @@
+function convert() {
+    const celsius = document.getElementById("celsius").value;
+    const fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit.toFixed(2);
+}
+
+function revealMessage() {
+    var message = document.getElementById("message");
+    message.style.display = "block";
+  }
+  
